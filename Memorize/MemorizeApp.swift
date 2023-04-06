d836cf83f51ceda8642c044a42d002e192cc4f4f
tochi-ukpai <tochukwuukpai@gmail.com>
2023-04-06 13:52:47 +0100

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Theós on 06/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
