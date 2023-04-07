2571c3de2e13deb99ea7b40b8e0fd062ed1cf231
tochi-ukpai <125578840+iamtheos@users.noreply.github.com>
2023-04-07 14:54:42 +0100

    Start preliminary design of cards view.

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,13 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
+        ZStack {
+            RoundedRectangle(cornerRadius: 20)
+                .stroke(lineWidth: 3)
             Text("Hello, world!")
         }
-        .padding()
+        .padding(.horizontal)
+        .foregroundColor(.red)
     }
 }
 
